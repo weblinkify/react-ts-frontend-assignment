d62ac39c9f4497ce0a114d37fe4e79727a03bea4
weblinkify <daniyaltariq09@gmail.com>
2024-09-25 14:29:38 +0300

    Type: Defined TypeScript types for User object

---
+++ src/types/user.ts
@@ -0,0 +1,15 @@
+export interface Address {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+}
+
+export interface User {
+  id: number;
+  name: string;
+  email: string;
+  phone: string;
+  website: string;
+  address: Address;
+}
